726f4da2ad62dc8e37fd9718d720f1bd015bce96
EshimovShaxzod <126951571+EshimovShaxzod@users.noreply.github.com>
2023-06-26 17:51:28 +0500

    change tab

---
+++ dom.js
@@ -0,0 +1,190 @@
+function myFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Choose description content';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'inline-block';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'inline-block';
+    let btn = document.getElementById('btn');
+    btn.style.display = 'none';
+    let span1 = document.getElementById('span1');
+    span1.classList.add('active');
+    let span = document.getElementById('span');
+    span.classList.remove('active');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.add('choose');
+    let choose1 = document.getElementById('choose1');
+    choose1.classList.remove('choose');
+}
+
+function subFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Are you happy now ?';
+    let btn3 = document.getElementById('btn3');
+    btn3.style.display = 'inline-block';
+    let btn4 = document.getElementById('btn4');
+    btn4.style.display = 'inline-block';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'none';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'none';
+    let span1 = document.getElementById('span1');
+    span1.classList.remove('active');
+    let span = document.getElementById('span');
+    span.classList.remove('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.add('active');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.remove('choose');
+    let choose3 = document.getElementById('choose3');
+    choose3.classList.add('choose');
+}
+
+function goFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Choose description content';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'inline-block';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'inline-block';
+    let btn = document.getElementById('btn');
+    btn.style.display = 'none';
+    let btn3 = document.getElementById('btn3');
+    btn3.style.display = 'none';
+    let btn4 = document.getElementById('btn4');
+    btn4.style.display = 'none';
+    let span1 = document.getElementById('span1');
+    span1.classList.add('active');
+    let span = document.getElementById('span');
+    span.classList.remove('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.remove('active');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.add('choose');
+    let choose3 = document.getElementById('choose3');
+    choose3.classList.remove('choose');
+}
+
+function backFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Choose title content';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'none';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'none';
+    let btn = document.getElementById('btn');
+    btn.style.display = 'inline-block';
+    let span1 = document.getElementById('span1');
+    span1.classList.remove('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.remove('active');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.remove('choose');
+    let span = document.getElementById('span');
+    span.classList.add('active');
+    let choose1 = document.getElementById('choose1');
+    choose1.classList.add('choose');
+}
+
+function yesFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = "ok, we're good";
+    let btn3 = document.getElementById('btn3');
+    btn3.style.display = 'none';
+    let btn4 = document.getElementById('btn4');
+    btn4.style.display = 'none';
+    let span1 = document.getElementById('span1');
+    span1.classList.remove('active');
+    let span = document.getElementById('span');
+    span.classList.remove('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.remove('active');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.remove('choose');
+    let choose3 = document.getElementById('choose3');
+    choose3.classList.remove('choose');
+}
+
+
+function fileFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Choose title content';
+    let btn = document.getElementById('btn');
+    btn.style.display = 'inline-block';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'none';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'none';
+    let btn3 = document.getElementById('btn3');
+    btn3.style.display = 'none';
+    let btn4 = document.getElementById('btn4');
+    btn4.style.display = 'none';
+    let span = document.getElementById('span');
+    span.classList.add('active');
+    let span1 = document.getElementById('span1');
+    span1.classList.remove('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.remove('active');
+    let choose1 = document.getElementById('choose1');
+    choose1.classList.add('choose');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.remove('choose');
+    let choose3 = document.getElementById('choose3');
+    choose3.classList.remove('choose');
+
+}
+
+
+function descFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Choose description content';
+    let btn = document.getElementById('btn');
+    btn.style.display = 'none';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'inline-block';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'inline-block';
+    let btn3 = document.getElementById('btn3');
+    btn3.style.display = 'none';
+    let btn4 = document.getElementById('btn4');
+    btn4.style.display = 'none';
+    let span = document.getElementById('span');
+    span.classList.remove('active');
+    let span1 = document.getElementById('span1');
+    span1.classList.add('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.remove('active');
+    let choose1 = document.getElementById('choose1');
+    choose1.classList.remove('choose');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.add('choose');
+    let choose3 = document.getElementById('choose3');
+    choose3.classList.remove('choose');
+}
+
+function dataFunction(){
+    let text = document.getElementById('demo');
+    text.innerHTML = 'Are you happy now ?';
+    let span = document.getElementById('span');
+    span.classList.remove('active');
+    let span1 = document.getElementById('span1');
+    span1.classList.remove('active');
+    let span2 = document.getElementById('span2');
+    span2.classList.add('active');
+    let btn = document.getElementById('btn');
+    btn.style.display = 'none';
+    let btn1 = document.getElementById('btn1');
+    btn1.style.display = 'none';
+    let btn2 = document.getElementById('btn2');
+    btn2.style.display = 'none';
+    let btn3 = document.getElementById('btn3');
+    btn3.style.display = 'inline-block';
+    let btn4 = document.getElementById('btn4');
+    btn4.style.display = 'inline-block';
+    let choose1 = document.getElementById('choose1');
+    choose1.classList.remove('choose');
+    let choose2 = document.getElementById('choose2');
+    choose2.classList.remove('choose');
+    let choose3 = document.getElementById('choose3');
+    choose3.classList.add('choose');
+}
